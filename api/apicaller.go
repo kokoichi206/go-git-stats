fd09ce7325e41ccb2ca48445d40ba7b65081bde2
takahiro.tominaga <tt.300607@gmail.com>
2022-08-08 02:10:01 +0900

    feat: public repositories (#2)

--- api/apicaller.go
+++ api/apicaller.go
@@ -1,6 +1,9 @@
 package api
 
 // Github REST Api caller
+// For detailed information, see the official documentations:
+// https://docs.github.com/ja/rest
 type ApiCaller interface {
+	ListPublicRepositories() ([]Repository, error)
 	ListRepositoriesForAuthenticatedUser() ([]Repository, error)
 }
