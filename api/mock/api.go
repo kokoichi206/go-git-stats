90b78a5783889047c4eaaacb5f306aeba5ceb903
takahiro.tominaga <tt.300607@gmail.com>
2022-08-08 22:33:10 +0900

    test: stats command (#5)

--- api/mock/api.go
+++ api/mock/api.go
@@ -8,15 +8,19 @@ import (
 type MockApi struct {
 	config              util.Config
 	ListRepos           []api.Repository
+	ListCodeFreq        []api.CodeFrequency
 	Error               error
 	PublicCalled        bool
 	AuthenticatedCalled bool
+	WeeklyCodeCalled    bool
+	PassedFullName      string
 }
 
 func (a *MockApi) InitMock() {
 	a.Error = nil
 	a.PublicCalled = false
 	a.AuthenticatedCalled = false
+	a.WeeklyCodeCalled = false
 }
 
 func (a *MockApi) ListPublicRepositories(userName string) ([]api.Repository, error) {
@@ -27,6 +31,12 @@ func (a *MockApi) ListPublicRepositories(userName string) ([]api.Repository, err
 func (a *MockApi) ListRepositoriesForAuthenticatedUser() ([]api.Repository, error) {
 	a.AuthenticatedCalled = true
 	return a.ListRepos, a.Error
+}
+
+func (a *MockApi) WeeklyCommitActivity(fullName string) ([]api.CodeFrequency, error) {
+	a.WeeklyCodeCalled = true
+	a.PassedFullName = fullName
+	return a.ListCodeFreq, a.Error
 }
 
 func New(config util.Config) *MockApi {
