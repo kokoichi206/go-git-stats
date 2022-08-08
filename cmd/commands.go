1fbebe8a110ece3cd9ac669ab4b8e9e5324f239b
takahiro.tominaga <tt.300607@gmail.com>
2022-08-08 12:31:06 +0900

    feat: repositories subcommand (#2)

---
+++ cmd/commands.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"github.com/kokoichi206/go-git-stats/api"
+	"github.com/kokoichi206/go-git-stats/util"
+	"github.com/urfave/cli/v2"
+)
+
+// struct that has information related to subcommands
+type Cmd struct {
+	Config util.Config
+	Api    *api.Api
+}
+
+// Get all commands.
+func (c *Cmd) NewCommands() []*cli.Command {
+	return []*cli.Command{
+		c.RepoCommand(),
+	}
+}
