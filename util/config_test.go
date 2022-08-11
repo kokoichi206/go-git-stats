c37d7a390f59b7cab5ae5f04a28125a420d0dd7a
takahiro.tominaga <tt.300607@gmail.com>
2022-08-11 14:41:54 +0900

    test: token validation (#6)

--- util/config_test.go
+++ util/config_test.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
@@ -12,19 +13,20 @@ func TestLoadConfig(t *testing.T) {
 	testCases := []struct {
 		name      string
 		setup     func()
-		assertion func(t *testing.T, config Config)
+		assertion func(t *testing.T, config Config, err error)
 		tearDown  func()
 	}{
 		{
 			name: "OK",
 			setup: func() {
-				os.Setenv("GGS_TOKEN", "ghq_foobarTOKEN")
+				os.Setenv("GGS_TOKEN", "ghq_q5k0u9YD8JPHVIUckyx4dKDyvGavdJWHR44D")
 			},
-			assertion: func(t *testing.T, config Config) {
+			assertion: func(t *testing.T, config Config, err error) {
 				t.Log(config)
 				// GitHub REST API
 				require.Equal(t, "https://api.github.com", config.ApiBaseURL)
-				require.Equal(t, "ghq_foobarTOKEN", config.Token)
+				require.Equal(t, "ghq_q5k0u9YD8JPHVIUckyx4dKDyvGavdJWHR44D", config.Token)
+				require.NoError(t, err)
 			},
 			tearDown: func() {
 				os.Unsetenv("GGS_TOKEN")
@@ -33,13 +35,30 @@ func TestLoadConfig(t *testing.T) {
 		{
 			name:  "OK without token",
 			setup: func() {},
-			assertion: func(t *testing.T, config Config) {
+			assertion: func(t *testing.T, config Config, err error) {
 				t.Log(config)
 				// GitHub REST API
 				require.Equal(t, "https://api.github.com", config.ApiBaseURL)
 				require.Equal(t, "", config.Token)
+				require.NoError(t, err)
 			},
 			tearDown: func() {},
+		},
+		{
+			name: "Token format error",
+			setup: func() {
+				os.Setenv("GGS_TOKEN", "invalid_token")
+			},
+			assertion: func(t *testing.T, config Config, err error) {
+				t.Log(config)
+				require.Equal(t, "", config.ApiBaseURL)
+				require.Equal(t, "", config.Token)
+				require.Error(t, err)
+				require.Equal(t, err.Error(), fmt.Sprintf("Your token: '%s' is invalid format.\nPlease check your environment variable [GGS_TOKEN].", "invalid_token"))
+			},
+			tearDown: func() {
+				os.Unsetenv("GGS_TOKEN")
+			},
 		},
 	}
 
@@ -52,10 +71,59 @@ func TestLoadConfig(t *testing.T) {
 			defer tc.tearDown()
 
 			// Act
-			config := LoadConfig()
+			config, err := LoadConfig()
 
 			// Assert
-			tc.assertion(t, config)
+			tc.assertion(t, config, err)
+		})
+	}
+}
+
+func TestIsValidFormat(t *testing.T) {
+
+	testCases := []struct {
+		name     string
+		token    string
+		expected bool
+	}{
+		{
+			name:     "OK",
+			token:    "ghq_6nKaaa1xmIdPQ0jU9fZwOMx97qjI6343PzoT",
+			expected: true,
+		},
+		{
+			name:     "NG wrong prefix",
+			token:    "ghr_6nKaaa1xmIdPQ0jU9fZwOMx97qjI6343PzoT",
+			expected: false,
+		},
+		{
+			name:     "NG short",
+			token:    "ghq_6nKaaa",
+			expected: false,
+		},
+		{
+			name:     "NG wrong prefix",
+			token:    "ghr_6nKaaa1xmIdPQ0jU9fZwOMx97qjI6343PzoTKp43jfafeapjdpl",
+			expected: false,
+		},
+		{
+			name:     "NG wrong character",
+			token:    "ghr_wrong;character;jU9fZwOMxqjI6343PzoT",
+			expected: false,
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			// Arrange
+
+			// Act
+			result := isValidFormat(tc.token)
+
+			// Assert
+			require.Equal(t, tc.expected, result)
 		})
 	}
 }
